cba197122edae0508b7c32266b49bffc24fadb91
AmirHesamB <amirh3sam.badiei@gmail.com>
2023-07-30 11:25:03 -0400

    update

---
+++ src/main/java/com/compnay/MainClass.java
@@ -0,0 +1,14 @@
+package com.compnay;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class MainClass {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("TestA");
+    }
+
+}
